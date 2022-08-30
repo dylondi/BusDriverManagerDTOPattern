c3d7b35e05a50450a93ada7228cdd5a0cb78c622
dylondi <dylon.diruscio@hotmail.com>
2022-08-30 06:21:18 +0200

    Initial commit

---
+++ src/test/java/com/example/busdrivermanager/BusDriverManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busdrivermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusDriverManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
